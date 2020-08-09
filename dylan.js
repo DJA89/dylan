8c7de50dfbc19c775cf8278ba70fc8171503aadb
DJA <drod89@gmail.com>
2020-08-09 20:58:46 -0300

    Tidying up a little

--- dylan.js
+++ dylan.js
@@ -1,14 +1,46 @@
 // First argument the user credentials
 // Second optional argument discography relative file path
 
+
+const axios = require('axios').default;
+const  querystring = require('querystring');
+
+async function get(url, params, headers, identifier) {
+  let response;
+  try {
+    response = await axios.get(url, {
+        params: params,
+       headers: headers,
+     }).catch(function (error) {
+       console.log(error.request);
+       console.log(`Request error on getting ${identifier}: ${error}`)
+     });
+  } catch (err) {
+    console.log(`Application error on getting ${identifier}: ${err}`);
+  };
+  return response;
+}
+
+function sorter(a, b) {
+  if (a[0] != b[0]) {
+    return (a[0] - b[0]);
+  } else {
+    if (a[1] < b[1]) {return -1;}
+    if (a[1] > b[1]) {return 1;}
+    return 0;
+  }
+};
+
 async function main() {
   const SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token';
   const SPOTIFY_ALBUMS_URL = 'https://api.spotify.com/v1/albums';
   const SPOTIFY_SEARCH_URL = 'https://api.spotify.com/v1/search';
   const SPOTIFY_ARTISTS_URL = 'https://api.spotify.com/v1/artists';
 
+  const TRELLO_BASE_URL = 'https://api.trello.com/1/';
+
   const our_path = process.argv[1];
-  const credentials = process.argv[2];
+  const credentials = process.argv[2] || process.env.TRELLO_USER_TOKEN;
   const file_path = process.argv[3] || 'discography.txt';
 
   fs = require('fs');
@@ -18,25 +50,13 @@ async function main() {
 
   let discography = fs.readFileSync(discography_path, 'utf8')
 
-  discography = discography.split('\n').filter((disk) => disk).map((disk) => (disk.match(/^(\d*) (.*)$/).slice(1, 3))).sort((a, b) => {
-    if (a[0] != b[0]) {
-      return (a[0] - b[0]);
-    } else {
-      if (a[1] < b[1]) {return -1;}
-      if (a[1] > b[1]) {return 1;}
-      return 0;
-    }
-  });
+  discography = discography.split('\n').filter((disk) => disk).map((disk) => (disk.match(/^(\d*) (.*)$/).slice(1, 3))).sort(sorter);
 
   require('dotenv').config();
 
   const client_id = process.env.SPOTIFY_CLIENT_ID;
   const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
   const encoded = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
-
-  const axios = require('axios').default;
-  const  querystring = require('querystring');
-
 
   // Get the token from spotify
   let response;
@@ -58,76 +78,46 @@ async function main() {
   const spotify_token = response.data.access_token;
 
   // Get albums from spotify
-  try {
-    response = await axios.get(SPOTIFY_SEARCH_URL, {
-        params: {
-            q: 'bob dylan',
-            type: 'artist',
-            limit: 1,
-        },
-       headers: {
-         'Accept': 'application/json',
-         'Content-Type': 'application/json',
-         'Authorization': `Bearer ${spotify_token}`
-       }
-     }).catch(function (error) {
-       console.log(error.request);
-       console.log(`Request error on getting artist from spotify: ${error}`)
-     });
-  } catch (err) {
-    console.log(`Application error on getting artist from spotify: ${err}`);
-  }
+  const artist_id = (await get(
+                      SPOTIFY_SEARCH_URL, {
+                        q: 'bob dylan',
+                        type: 'artist',
+                        limit: 1,
+                      }, {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${spotify_token}`
+                      },
+                      'artist from spotify'
+                    )).data.artists.items[0].id;
 
-  const artist_id = response.data.artists.items[0].id;
+  const first_albums_images = (await get(
+                                `${SPOTIFY_ARTISTS_URL}/${artist_id}/albums`, {
+                                  limit: 50,
+                                }, {
+                                  'Accept': 'application/json',
+                                  'Content-Type': 'application/json',
+                                  'Authorization': `Bearer ${spotify_token}`
+                                },
+                                'albums from spotify'
+                              )).data.items;
 
-  try {
-    response = await axios.get(`${SPOTIFY_ARTISTS_URL}/${artist_id}/albums`, {
-        params: {
-            limit: 50,
-        },
-       headers: {
-         'Accept': 'application/json',
-         'Content-Type': 'application/json',
-         'Authorization': `Bearer ${spotify_token}`
-       }
-     }).catch(function (error) {
-       console.log(error.request);
-       console.log(`Request error on getting albums from spotify: ${error}`)
-     });
-  } catch (err) {
-    console.log(`Application error on getting albums from spotify: ${err}`);
-  }
 
-  const first_albums_images = response.data.items;
+  const second_albums_images = (await get(
+                                `${SPOTIFY_ARTISTS_URL}/${artist_id}/albums`, {
+                                  limit: 50,
+                                  offset: 50
+                                }, {
+                                  'Accept': 'application/json',
+                                  'Content-Type': 'application/json',
+                                  'Authorization': `Bearer ${spotify_token}`
+                                },
+                                'albums from spotify'
+                              )).data.items;
 
-  try {
-    response = await axios.get(`${SPOTIFY_ARTISTS_URL}/${artist_id}/albums`, {
-        params: {
-            limit: 50,
-            offset: 50
-        },
-       headers: {
-         'Accept': 'application/json',
-         'Content-Type': 'application/json',
-         'Authorization': `Bearer ${spotify_token}`
-       }
-     }).catch(function (error) {
-       console.log(error.request);
-       console.log(`Request error on getting albums from spotify: ${error}`)
-     });
-  } catch (err) {
-    console.log(`Application error on getting albums from spotify: ${err}`);
-  }
+  const albums_images  = [...first_albums_images, ...second_albums_images].map((disk) => ([disk.release_date.substring(0, 4), disk.name, disk.images])).sort(sorter);
 
-  const albums_images  = [...first_albums_images, ...response.data.items].map((disk) => ([disk.release_date.substring(0, 4), disk.name, disk.images])).sort((a, b) => {
-    if (a[0] != b[0]) {
-      return (a[0] - b[0]);
-    } else {
-      if (a[1] < b[1]) {return -1;}
-      if (a[1] > b[1]) {return 1;}
-      return 0;
-    }
-  });
+
   // Recorrer el segundo array y chequear si esta en el primeroi
   // console.log('From file');
   // console.log(discography.map((disk) => (`${disk[0]} - ${disk[1]}`)));
